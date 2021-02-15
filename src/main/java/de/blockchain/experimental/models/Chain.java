d98e6327cfc74422aa7bd0856455a3290194470c
mirko-pleli <mirko.pleli@outlook.de>
2021-02-15 21:10:46 +0100

    Add models and helper classes for logical chain.

---
+++ src/main/java/de/blockchain/experimental/models/Chain.java
@@ -0,0 +1,54 @@
+package de.blockchain.experimental.models;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Chain {
+
+    private List<Block> chain = new CopyOnWriteArrayList<>( );
+
+    private int networkId;
+
+    public void add( Block block )
+    {
+        chain.add( block );
+    }
+
+    public Block get( int index )
+    {
+        return chain.get( index );
+    }
+
+    public Block getLast( )
+    {
+        return chain.get( chain.size( ) - 1 );
+    }
+
+    public int size( )
+    {
+        return chain.size( );
+    }
+
+    public List<Block> getChain( )
+    {
+        return chain;
+    }
+
+    public int getNetworkId( )
+    {
+        return networkId;
+    }
+
+    public void setNetworkId( int networkId )
+    {
+        this.networkId = networkId;
+    }
+
+    @Override public String toString( )
+    {
+        return "Chain{" +
+                "chain=" + chain +
+                ", networkId=" + networkId +
+                '}';
+    }
+}

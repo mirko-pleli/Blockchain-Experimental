b409b12a775e4e52d3c77642e44a06c76ac60c30
mirko-pleli <mirko.pleli@outlook.de>
2021-02-15 21:30:11 +0100

    Formatting

--- src/main/java/de/blockchain/experimental/models/Chain.java
+++ src/main/java/de/blockchain/experimental/models/Chain.java
@@ -5,47 +5,40 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Chain {
 
-    private List<Block> chain = new CopyOnWriteArrayList<>( );
+    private List<Block> chain = new CopyOnWriteArrayList<>();
 
     private int networkId;
 
-    public void add( Block block )
-    {
-        chain.add( block );
+    public void add(Block block) {
+        chain.add(block);
     }
 
-    public Block get( int index )
-    {
-        return chain.get( index );
+    public Block get(int index) {
+        return chain.get(index);
     }
 
-    public Block getLast( )
-    {
-        return chain.get( chain.size( ) - 1 );
+    public Block getLast() {
+        return chain.get(chain.size() - 1);
     }
 
-    public int size( )
-    {
-        return chain.size( );
+    public int size() {
+        return chain.size();
     }
 
-    public List<Block> getChain( )
-    {
+    public List<Block> getChain() {
         return chain;
     }
 
-    public int getNetworkId( )
-    {
+    public int getNetworkId() {
         return networkId;
     }
 
-    public void setNetworkId( int networkId )
-    {
+    public void setNetworkId(int networkId) {
         this.networkId = networkId;
     }
 
-    @Override public String toString( )
-    {
+    @Override
+    public String toString() {
         return "Chain{" +
                 "chain=" + chain +
                 ", networkId=" + networkId +
